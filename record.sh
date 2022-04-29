67db9730f9170712d5d98eff6e035eb078765e81
Nick O'Connor <36704600+noconnor29@users.noreply.github.com>
2022-04-29 00:03:54 -0400

    reorganized and archived dev files

--- record.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-now=`date +"%m-%d-%Y-%H"`
-#END=24
-#for ((i=1;i<=END;i++)); do
-#	arecord -D plughw:1,0 -c 1 -f cd -d 3600 | lame - audio.mp3
-#done
-#cd /home/pi/datasets/sound-detect/audio
-arecord -D plughw:1,0 -c 1 -f cd -d 60 | lame - /home/pi/datasets/sound-detect/audio/${now}.mp3
--- start_recording.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pkill -f timestamp
-ps ax | grep ffmpeg | awk -F ' ' '{print $1}}' | xargs sudo kill -9
-sleep 60
-/usr/bin/python3 -u /home/pi/rooster_recorder/timestamp_predict_from_microphone.py >> /home/pi/rooster_recorder/datasets/sound-detect/logs/`date +%m-%d-%Y`.log 2>&1 |/usr/bin/logger -t ROOSTER 
