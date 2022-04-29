a5ffc935e51f79aad4fa716d4307d3c4da8be658
Nick O'Connor <36704600+noconnor29@users.noreply.github.com>
2022-04-28 23:49:16 -0400

    fixed typo and corrected service target

--- rooster_recorder.py
+++ rooster_recorder.py
@@ -9,6 +9,7 @@ from dateutil import tz
 # Path to a previously saved sound detection Tensorflow model
 model_dir = os.path.expanduser('~/rooster_recorder/models/sound-detect')
 model = Model.load(model_dir)
+log_dir = os.path.expanduser('~/rooster_recorder/datasets/sound-detect/logs')
 
 audio_system = 'alsa'        # Supported: alsa and pulse
 audio_device = 'plughw:1,0'  # Get list of recognized input devices with arecord -l
@@ -20,7 +21,7 @@ with AudioDevice(audio_system, device=audio_device) as source:
         source.pause()  # Pause recording while we process the frame
         prediction = model.predict(sample)
         if prediction == "positive":
-            with oprn(os.path.join(model_dir, "logs", str(date + ".log")), "a") as file:
+            with open(os.path.join(log_dir, str(date + ".log")), "a") as file:
                 file.write(str(prediction + " " + dt_string))
         time.sleep(3) # Detection cooldown
         source.resume() # Resume recording
