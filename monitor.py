36954cceb9f5345e5ba174ebb5d2a5b5b2f9117c
Nick O'Connor <36704600+noconnor29@users.noreply.github.com>
2022-05-02 23:35:45 -0400

    added back in logfile for testing period

--- monitor.py
+++ monitor.py
@@ -7,10 +7,14 @@ from micmon.model import Model
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+## Remove later
+from datetime import datetime
+from dateutil import tz
+log_dir = os.path.expanduser('~/rooster_recorder/datasets/sound-detect/logs')
+
 # Path to a previously saved sound detection Tensorflow model
 model_dir = os.path.expanduser('~/rooster_recorder/models/sound-detect')
 model = Model.load(model_dir)
-log_dir = os.path.expanduser('~/rooster_recorder/datasets/sound-detect/logs')
 
 AUDIO_SYSTEM = 'alsa'        # Supported: alsa and pulse
 AUDIO_DEVICE = 'plughw:1,0'  # Get list of recognized input devices with arecord -l
@@ -22,6 +26,9 @@ BUCKET = "rooster_metrics"
 
 with AudioDevice(AUDIO_SYSTEM, device=AUDIO_DEVICE) as source:
     for sample in source:
+        now = datetime.now(tz = tz.tzlocal())
+        dt_string = now.strftime("%m-%d-%Y %H:%M:%S\n")
+        date = now.strftime("%m-%d-%Y")
         source.pause()  # Pause recording while we process the frame
         prediction = model.predict(sample)
         if prediction == "positive":
@@ -30,5 +37,7 @@ with AudioDevice(AUDIO_SYSTEM, device=AUDIO_DEVICE) as source:
                 DATA = "crow detection=True"
                 write_api.write(BUCKET, ORG, DATA, time_precision='s')
                 client.close()
+            with open(os.path.join(log_dir, str(date + ".log")), "a") as file:
+                file.write(str(prediction + " " + dt_string))
         time.sleep(3) # Detection cooldown
         source.resume() # Resume recording
